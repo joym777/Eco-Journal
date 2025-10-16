3e14d08d41eeea736ade86e6057987fc216c9639
Joy <joymuteti22@gmail.com>
2025-10-17 00:30:24 +0300

    added arrat template for quotes to js page

---
+++ Scripts/vision.js
@@ -0,0 +1,27 @@
+//quote generator for vision page after every page reload
+const ecoQuote = [ //creating an array to store all quotes and authors
+  {
+    text: "If children do not grow up knowing about nature & appreciating it, they will not undrstand it. And if they do not understand it, they won't protect it."
+    author: "-David Attenborough"
+  },
+   {
+    text: "We are the first generation to feel the impact of climate change, and the last generation that can do something about it."
+    author: ""
+  },
+   {
+    text: ""
+    author: ""
+  },
+   {
+    text: ""
+    author: ""
+  },
+   {
+    text: ""
+    author: ""
+  },
+   {
+    text: ""
+    author: ""
+  },
+]
