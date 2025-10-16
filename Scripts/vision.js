2746ea04abca790122bed04dd5009a52ede6c4c0
Joy <joymuteti22@gmail.com>
2025-10-17 00:48:37 +0300

    added function to make quote selections random

--- Scripts/vision.js
+++ Scripts/vision.js
@@ -6,22 +6,30 @@ const ecoQuote = [ //creating an array to store all quotes and authors
   },
    {
     text: "We are the first generation to feel the impact of climate change, and the last generation that can do something about it."
-    author: ""
+    author: "-Barack Obama"
   },
    {
-    text: ""
-    author: ""
+    text: "The earth is what we all have in common"
+    author: "-Wendell Berry"
   },
    {
-    text: ""
-    author: ""
+    text: "If you really think the environment is less important than the economy, try holding your breath while counding your money."
+    author: "-Guy McPherson"
   },
    {
-    text: ""
-    author: ""
+    text: "The future will be gree, or not at all."
+    author: "Jonathon Porritt"
   },
    {
-    text: ""
-    author: ""
+    text: "When we heal the earth, we heal ourselves."
+    author: "-David Orr"
   },
-]
+];
+//function to randomly select a quote from the array above
+function getRandomQuote () { 
+  //selects a random decimal number between 0 and 1, then multiplies by 6 (number of qoutes available)
+  //Math.floor rounds down the digit result to match the index of a specific quote (0-5)
+  const randomIndex = Math.floor(Math.random() * ecoQuote.length); 
+  return ecoQuotes[randomIndex]; //returns the quote with the specific index calculated 
+
+}
